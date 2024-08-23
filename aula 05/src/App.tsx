7e300b2f044023c1e0bff8d9b80655279c9d8f2f
Caepena <caepenafiel@gmail.com>
2024-08-23 08:50:52 -0300

    Bagulin pronto ksksksks

--- aula 05/src/App.tsx
+++ aula 05/src/App.tsx
@@ -1,25 +1,27 @@
 import Cabecalho from "./Components/Cabecalho/Cabecalho";
-import Conteudo from "./Components/Conteudo/Conteudo";
-import Rodape from "./Components/Rodape/Rodape";
 
 export default function App() {
-  const pagina:string = "Minha página principal"
-  const nrPagina:number = 2
-  const status = "xuxu"
-  const aviso = ()=> alert("Alerta vindo do pai!!!")
-  // const numero: number = 2
-  // const listaDeNomes: string[] = ["João", "Maria", "Pedro"]
-  // const objeto: {nome: string, idade:number} = {nome: "João", idade: 25}
-  // const objeto2: {nome: string, idade:number, isEstudante?:boolean} = {nome: "Maria", idade: 30}
-  // const elementoDiv: HTMLDivElement = document.createElement('div')
 
+  const pagina: string = "Minha página principal!";
+  const nrPagina: string = "DOIS";
+  const status = "xuxu";
+  const aviso = () => alert(pagina);
 
-  return(
+  return (
     <div>
       <h1>Componente App</h1>
-      <Cabecalho paginaProps={pagina} nrPaginaProps={nrPagina} statusProps={status} avisoProps={aviso} />
-      <Conteudo/>
-      <Rodape/>
+      <Cabecalho paginaProps={pagina} nrPaginaProps={nrPagina} statusProps={status} avisoProps={aviso}>
+
+        {/* ul>li*5>a[href=#]{Aluno-$} */}
+        <ul>
+          <li><a href="#">Aluno-1</a></li>
+          <li><a href="#">Aluno-2</a></li>
+          <li><a href="#">Aluno-3</a></li>
+          <li><a href="#">Aluno-4</a></li>
+          <li><a href="#">Aluno-5</a></li>
+        </ul>
+
+      </Cabecalho>
     </div>
   );
 }
